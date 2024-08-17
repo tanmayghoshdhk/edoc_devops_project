7ebddd4e6d1c1d7352beb1ccb41ed2cc89e4456a
Tanmay Ghosh <tanmaydhk@gmail.com>
2024-08-17 12:18:13 +0600

    Update index.php

--- html/patient/index.php
+++ html/patient/index.php
@@ -1,3 +1,8 @@
+<?php
+    ob_start(); // Start output buffering
+
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -404,3 +409,6 @@
 
 </body>
 </html>
+<?php
+    ob_end_flush(); // End output buffering
+?>
