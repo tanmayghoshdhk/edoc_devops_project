d9c91002766981a9d5b1e3741543027970d58349
Tanmay Ghosh <tanmaydhk@gmail.com>
2024-08-17 12:26:42 +0600

    Update add-new.php

--- html/admin/add-new.php
+++ html/admin/add-new.php
@@ -1,6 +1,5 @@
 <?php
-
-    //learn from w3schools.com
+    ob_start(); // Start output buffering
 
     session_start();
 ?>
@@ -93,3 +92,6 @@
 
 </body>
 </html>
+<?php
+    ob_end_flush(); // End output buffering
+?>
