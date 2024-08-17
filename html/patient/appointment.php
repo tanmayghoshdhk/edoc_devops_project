2a674dbf64ab807acf3e6aef0467b3972d39d412
Tanmay Ghosh <tanmaydhk@gmail.com>
2024-08-17 12:11:52 +0600

    Update appointment.php
    Header update, session update

--- html/patient/appointment.php
+++ html/patient/appointment.php
@@ -1,3 +1,8 @@
+<?php
+    ob_start(); // Start output buffering
+
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -523,3 +528,6 @@
 
 </body>
 </html>
+<?php
+    ob_end_flush(); // End output buffering
+?>
