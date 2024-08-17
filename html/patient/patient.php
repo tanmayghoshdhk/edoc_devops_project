107a0e529ff82093834a4b67d747e561c9ff4a96
Tanmay Ghosh <tanmaydhk@gmail.com>
2024-08-17 12:18:52 +0600

    Update patient.php

--- html/patient/patient.php
+++ html/patient/patient.php
@@ -1,3 +1,8 @@
+<?php
+    ob_start(); // Start output buffering
+
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -483,3 +488,6 @@
 
 </body>
 </html>
+<?php
+    ob_end_flush(); // End output buffering
+?>
